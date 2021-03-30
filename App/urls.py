05062b4216e99e7ddbf100bbf1e48d818a591cfa
syrine868 <syrine868@gmail.com>
2021-03-30 15:39:55 +0100

    LOGIN & REGISTER FORMS WITH ACCUEIL

--- App/urls.py
+++ App/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from . import views
 from .views import *
 
@@ -7,8 +7,12 @@ urlpatterns = [
     path('index/', views.index, name='index'),
     path('projet/<int:id>/', views.projet_id),
     path('/', views.projet),
-    path('liste/', views.liste_projet),
+    path('liste/', views.liste_projet, name='liste'),
     path('detail/<int:id>/', views.detail, name="D"),
     path('ListView/', projet_liste.as_view()),
-    path('DetailView/<int:pk>/', projet_detail.as_view(), name="detail")
+    path('DetailView/<int:pk>/', projet_detail.as_view(), name="detail"),
+    path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name="login"),
+    path('logout/', views.logout_user, name='logout'),
+    path('signup/', views.signup, name='signup'),
+    path('accueil/', views.accueil, name='accueil')
 ]
--- App/views.py
+++ App/views.py
@@ -1,5 +1,8 @@
+from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import *
 
 # Create your views here.
@@ -18,6 +21,7 @@ def projet(request):
     return render(request, 'App/index.html')
 
 
+# @login_required(login_url='login')
 def liste_projet(request):
     projets = Projet.objects.all()
     context = {'p': projets}
@@ -36,6 +40,30 @@ class projet_liste(ListView):
     context_object_name = 'p'
     # pour inverser l'ordre m asc l desc nzid -
     ordering = ['-temps_alloue']
+
+
 class projet_detail(DetailView):
     model = Projet
     context_object_name = 'pp'
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # user = authenticate()
+            # login(user)
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'user/signup.html', {'f': form})
+
+
+def accueil(request):
+    return render(request, 'accueil.html')
